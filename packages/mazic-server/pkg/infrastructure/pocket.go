61d048966a67d8c7994ef92ac887d4a1eb9c39d3
phidn <phidnhh@gmail.com>
2024-10-09 16:34:24 +0700

    feat(pocketbase): added migrations

--- packages/mazic-server/pkg/infrastructure/pocket.go
+++ packages/mazic-server/pkg/infrastructure/pocket.go
@@ -1,7 +1,11 @@
 package infrastructure
 
 import (
+	"os"
+	"strings"
+
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 type Pocket struct {
@@ -10,6 +14,14 @@ type Pocket struct {
 
 func NewPocket() *Pocket {
 	app := pocketbase.New()
+
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+
 	return &Pocket{app}
 }
 
